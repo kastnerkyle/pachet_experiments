4468fb0d7a79cccfca6be3f2611c7c1a497ae795
Kyle Kastner <kastnerkyle@gmail.com>
2017-07-29 01:08:27 -0400

    Working version of SparseMaxEnt

--- maxent_tests/maxent.py
+++ maxent_tests/maxent.py
@@ -1,6 +1,7 @@
 import numpy as np
 from collections import Counter, defaultdict
 from minimize import minimize
+import scipy as sp
 
 def log_sum_exp(x, axis=-1):
     """Compute log(sum(exp(x))) in a numerically stable way.
@@ -22,140 +23,31 @@ def log_sum_exp(x, axis=-1):
         return x_max + np.log(np.sum(np.exp(x - x_max[..., None]), last))
 
 
-class MaxEnt(object):
-    """ Also called a log-linear model, or logistic regression. """
-    def __init__(self, feature_function, n_features, n_classes, smoothing=0,
-                 verbose=True):
-        # feature function returns list of indices
-        # features are only indicator
-        # assume sparse setup
-        self.n_features = n_features
-        self.n_classes = n_classes
-        self.params = np.zeros((self.n_classes * self.n_features + self.n_classes,))
-        self.weights = self.params[:self.n_classes * self.n_features].reshape(self.n_features, self.n_classes)
-        self.biases = self.params[-self.n_classes:]
-        self.feature_function = feature_function
-        self.class_feature_counters = [Counter() for c in range(n_classes)]
-        self.class_priors = [0 for c in range(n_classes)]
-        self.class_defaults = [defaultdict(lambda: smoothing) for c in range(n_classes)]
-        self.verbose = verbose
-
-    def fit(self, data, labels, weight_cost):
-        max_n_line_search = np.inf
-        p, g, n_line_searches = minimize(self.params.copy(),
-                                         (data, labels, weight_cost),
-                                         self.f,
-                                         self.g,
-                                         maxnumlinesearch=max_n_line_search,
-                                         verbose=self.verbose)
-        if self.verbose:
-            print("Training complete!")
-            self.update_params(p)
-
-    def _oh(self, x, max_classes=None):
-        if max_classes == None:
-            n_classes = self.n_classes
-        else:
-            n_classes = max_classes
-        #list of list == lol
-        # need to normalize...
-        try:
-            max_len = max([len(xi) for xi in x])
-            empty = np.zeros((len(x), max_len)) - 1
-            for n, xi in enumerate(x):
-                empty[n, :len(xi)] = xi
-        except TypeError:
-            max_len = 1
-            empty = np.zeros((len(x), max_len)) - 1
-            for n, xi in enumerate(x):
-                empty[n] = xi
-
-        result = np.zeros([len(x)] + [n_classes], dtype="int")
-        z = np.zeros(len(x)).astype("int64")
-        for c in range(n_classes):
-            z *= 0
-            z[np.where(empty == c)[0]] = 1
-            result[..., c] += z
-        return result
-
-    def _uh(self, oh_x):
-        return oh_x.argmax(len(oh_x.shape)-1)
-
-    def predict_proba(self, data):
-        transform_data = [sorted(list(set(self.feature_function(d)))) for d in data]
-        max_classes = max([max(li) for li in transform_data]) + 1
-        oh_data = self._oh(transform_data, max_classes)
-        scores = np.dot(oh_data, self.weights) + self.biases[None, :]
-        probs = np.exp(scores - log_sum_exp(scores, 0))
-        return probs
-
-    def _cost(self, data, labels, weight_cost):
-        transform_data = [sorted(list(set(self.feature_function(d)))) for d in data]
-        max_classes = max([max(li) for li in transform_data]) + 1
-        oh_data = self._oh(transform_data, max_classes)
-
-        max_labels = max(labels) + 1
-        oh_labels = self._oh(labels, max_labels)
-        scores = np.dot(oh_data, self.weights) + self.biases[None, :]
-        nll = -((oh_labels * scores).sum() + log_sum_exp(scores, 0).sum())
-        nll = nll / float(oh_data.shape[1]) + weight_cost * np.sum(self.weights ** 2).sum()
-        if self.verbose:
-            print("nll {}".format(nll))
-        return nll
-
-    def _grads(self, data, labels, weight_cost):
-        grad_w = np.zeros((self.n_classes, self.n_features))
-        grad_b = np.zeros((self.n_classes,))
-        transform_data = [sorted(list(set(self.feature_function(d)))) for d in data]
-        max_classes = max([max(li) for li in transform_data]) + 1
-        oh_data = self._oh(transform_data, max_classes)
-
-        max_labels = max(labels) + 1
-        oh_labels = self._oh(labels, max_labels)
-        scores = np.dot(oh_data, self.weights) + self.biases[None, :]
-        probs = np.exp(scores - log_sum_exp(scores, 0))
-        for c in range(self.n_classes):
-            grad_w[c, :] = -np.sum((oh_labels[:, c] - probs[:, c])[:, None] * oh_data, 0)
-            grad_b[c] = -np.sum(oh_labels[:, c] - probs[:, c])
-        grad_w /= float(self.n_features)
-        grad_b /= float(self.n_features)
-        grad_w = grad_w.T + 2 * weight_cost * self.weights
-        grads = np.hstack((grad_w.flatten(), grad_b))
-        if self.verbose:
-            print("grads {}".format(grads.sum()))
-        return grads
-
-    def f(self, x, features, labels, weight_cost):
-        xold = self.params.copy()
-        self.update_params(x.copy())
-        result = self._cost(features, labels, weight_cost)
-        self.update_params(xold.copy())
-        return result
-
-    def g(self, x, features, labels, weight_cost):
-        """Wrapper function around gradient to check grads, etc."""
-        xold = self.params.copy()
-        self.update_params(x.copy())
-        result = self._grads(features, labels, weight_cost).flatten()
-        self.update_params(xold.copy())
-        return result
-
-    def update_params(self, new_params):
-        """ Update model parameters."""
-        self.params[:] = new_params.copy()
+def softmax(x):
+    if x.ndim == 1:
+        x = x.reshape((1, -1))
+    max_x = np.max(x, axis=1).reshape((-1, 1))
+    exp_x = np.exp(x - max_x)
+    return exp_x / np.sum(exp_x, axis=1).reshape((-1, 1))
 
 
-class SMaxEnt(object):
+class SparseMaxEnt(object):
     """ Also called a log-linear model, or logistic regression.
         Implementation using sparsity for discrete features"""
-    def __init__(self, feature_function, n_features, n_classes, smoothing=0,
+    def __init__(self, feature_function, n_features, n_classes,
+                 random_state=None, smoothing=0, optimizer="lbfgs",
                  verbose=True):
         # feature function returns list of indices
         # features are only indicator
         # assume sparse setup
         self.n_features = n_features
         self.n_classes = n_classes
-        self.params = np.zeros((self.n_classes * self.n_features + self.n_classes,))
+        self.random_state = random_state
+        self.optimizer = optimizer
+        if random_state == None:
+            raise ValueError("Random state must not be None!")
+        self.params = 0.02 * random_state.randn(self.n_classes * self.n_features + self.n_classes)
+        #self.params = np.zeros((self.n_classes * self.n_features + self.n_classes,))
         self.weights = self.params[:self.n_classes * self.n_features].reshape(self.n_features, self.n_classes)
         self.biases = self.params[-self.n_classes:]
         self.feature_function = feature_function
@@ -165,13 +57,22 @@ class SMaxEnt(object):
         self.verbose = verbose
 
     def fit(self, data, labels, weight_cost):
-        max_n_line_search = np.inf
-        p, g, n_line_searches = minimize(self.params.copy(),
-                                         (data, labels, weight_cost),
-                                         self.f_and_g,
-                                         True,
-                                         maxnumlinesearch=max_n_line_search,
-                                         verbose=self.verbose)
+        if self.optimizer == "lbfgs":
+            from scipy.optimize import minimize
+            res = minimize(self.f_and_g, self.params.copy(),
+                           (data, labels, weight_cost), method="L-BFGS-B", jac=True)
+            p = res.x
+        elif self.optimizer == "minimize_cg":
+            max_n_line_search = np.inf
+            p, g, n_line_searches = minimize(self.params.copy(),
+                                             (data, labels, weight_cost),
+                                             self.f_and_g,
+                                             True,
+                                             maxnumlinesearch=max_n_line_search,
+                                             verbose=self.verbose)
+        else:
+            raise ValueError("Unknown optimizer setting {}".format(self.optimizer))
+
         if self.verbose:
             print("Training complete!")
             self.update_params(p)
@@ -207,58 +108,19 @@ class SMaxEnt(object):
 
     def predict_proba(self, data):
         label_scores = np.zeros((len(data), self.n_classes))
-        # combine fwd and bwd
         for n, x in enumerate(data):
             active_idx = sorted(list(set(self.feature_function(x))))
             if len(active_idx) == 0:
                 continue
             active_weights = self.weights[active_idx, :]
-            active_biases = self.biases[None, :]
-            sscores = active_weights + active_biases
-            label_scores[n] = sscores.sum(axis=0)
-        sprobs = np.exp(label_scores - log_sum_exp(label_scores, 0))
+            active_biases = self.biases
+            sscores = active_weights.sum(axis=0) + active_biases
+            label_scores[n] = sscores
+        sprobs = softmax(label_scores)
         return sprobs
-
-    def _ohgrads(self, data, labels, weight_cost):
-        grad_w = np.zeros((self.n_classes, self.n_features))
-        grad_b = np.zeros((self.n_classes,))
-        transform_data = [sorted(list(set(self.feature_function(d)))) for d in data]
-        max_classes = max([max(li) for li in transform_data]) + 1
-        oh_data = self._oh(transform_data, max_classes)
-
-        max_labels = max(labels) + 1
-        oh_labels = self._oh(labels, max_labels)
-        scores = np.dot(oh_data, self.weights) + self.biases[None, :]
-        probs = np.exp(scores - log_sum_exp(scores, 0))
-        for c in range(self.n_classes):
-            grad_w[c, :] = -np.sum((oh_labels[:, c] - probs[:, c])[:, None] * oh_data, 0)
-            grad_b[c] = -np.sum(oh_labels[:, c] - probs[:, c])
-        grad_w /= float(self.n_features)
-        grad_b /= float(self.n_features)
-        grad_w = grad_w.T + 2 * weight_cost * self.weights
-        grads = np.hstack((grad_w.flatten(), grad_b))
-        if self.verbose:
-            print("grads {}".format(grads.sum()))
-        return grads
-
-    def _ohcost(self, data, labels, weight_cost):
-        transform_data = [sorted(list(set(self.feature_function(d)))) for d in data]
-        max_classes = max([max(li) for li in transform_data]) + 1
-        oh_data = self._oh(transform_data, max_classes)
-
-        max_labels = max(labels) + 1
-        oh_labels = self._oh(labels, max_labels)
-        scores = np.dot(oh_data, self.weights) + self.biases[None, :]
-        nll = -((oh_labels * scores).sum() + log_sum_exp(scores, 0).sum())
-        nll = nll / float(oh_data.shape[1]) + weight_cost * np.sum(self.weights ** 2).sum()
-        if self.verbose:
-            print("nll {}".format(nll))
-        return nll
 
     def _cost_and_grads(self, data, labels, weight_cost):
         assert len(data) == len(labels)
-        max_labels = max(labels) + 1
-        oh_labels = self._oh(labels, max_labels)
         label_scores = np.zeros((len(data), self.n_classes))
         for n, (x, y) in enumerate(zip(data, labels)):
             active_idx = sorted(list(set(self.feature_function(x))))
@@ -268,28 +130,28 @@ class SMaxEnt(object):
             active_biases = self.biases
             sscores = active_weights.sum(axis=0) + active_biases
             label_scores[n] = sscores
-        nll = -((oh_labels * label_scores).sum() + log_sum_exp(label_scores, 0).sum())
-        nll = nll / float(self.n_features) + weight_cost * np.sum(self.weights ** 2).sum()
+        sprobs = softmax(label_scores)
+        nll = -np.sum(np.log(sprobs)[list(range(len(labels))), labels])
+        nll = nll / float(len(labels)) + weight_cost * np.sum(self.weights ** 2).sum()
         if self.verbose:
             print("nll {}".format(nll))
-        nll = self._ohcost(data, labels, weight_cost)
 
-        sgrad_w = np.zeros((self.n_classes, self.n_features))
+        # see non-sparse derivation http://cs231n.github.io/neural-networks-case-study/#loss
+        dsprobs = sprobs
+        dsprobs[list(range(len(labels))), labels] -= 1
+        dsprobs /= float(len(labels))
+
+        sgrad_w = np.zeros((self.n_features, self.n_classes))
         sgrad_b = np.zeros((self.n_classes,))
-        sprobs = np.exp(label_scores - log_sum_exp(label_scores, 0))
         for n, (x, y) in enumerate(zip(data, labels)):
             active_idx = sorted(list(set(self.feature_function(x))))
             if len(active_idx) == 0:
                 continue
-            for c in range(self.n_classes):
-                sgrad_w[c, active_idx] += -(oh_labels[n, c] - sprobs[n, c])
-                sgrad_b[c] += -(oh_labels[n, c] - sprobs[n, c])
-        sgrad_w /= float(self.n_features)
-        sgrad_b /= float(self.n_features)
+            sgrad_w[active_idx] += dsprobs[n]
+            sgrad_b += dsprobs[n]
         grads = np.hstack((sgrad_w.flatten(), sgrad_b))
         if self.verbose:
-            print("grads_sum {}".format(grads.sum()))
-        grads = self._ohgrads(data, labels, weight_cost)
+            print("grads_norm {}".format(np.sqrt((grads ** 2).sum())))
         return nll, grads
 
     def f_and_g(self, x, features, labels, weight_cost):
--- maxent_tests/test_spam.py
+++ maxent_tests/test_spam.py
@@ -1,68 +1,77 @@
 import zipfile
 from tokenizing import Tokenizer
 from collections import Counter, defaultdict
-from maxent import MaxEnt, SMaxEnt
+from maxent import SparseMaxEnt
 import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+# based on https://stackoverflow.com/questions/23455728/scikit-learn-balanced-subsampling
+def balanced_sample_maker(X, y, sample_size="max", random_state=None):
+    y_o = y
+    y = np.array(y)
+    uniq_levels = np.unique(y)
+    uniq_counts = {level: sum(y == level) for level in uniq_levels}
+    if sample_size == "max":
+        sample_size = max([v for v in uniq_counts.values()])
+    if random_state is None:
+        raise ValueError("Must provide random state")
+
+    # find observation index of each class levels
+    groupby_levels = {}
+    for ii, level in enumerate(uniq_levels):
+        obs_idx = [idx for idx, val in enumerate(y) if val == level]
+        groupby_levels[level] = obs_idx
+
+    # oversampling on observations of each label
+    balanced_copy_idx = []
+    for gb_level, gb_idx in groupby_levels.iteritems():
+        if len(gb_idx) >= sample_size:
+            over_sample_idx = []
+            over_sample_idx += gb_idx
+        else:
+            over_sample_idx = []
+            # every minority datapoint at least once
+            over_sample_idx += gb_idx
+            ex_over_sample_idx = np.random.choice(gb_idx, size=sample_size - len(over_sample_idx), replace=True).tolist()
+            over_sample_idx += ex_over_sample_idx
+        balanced_copy_idx += over_sample_idx
+    np.random.shuffle(balanced_copy_idx)
+    X_bal = [X[idx] for idx in balanced_copy_idx]
+    y_bal = [y_o[idx] for idx in balanced_copy_idx]
+    return (X_bal, y_bal, balanced_copy_idx)
+
 
 fname = "smsspamcollection.zip"
 archive = zipfile.ZipFile(fname, 'r')
 raw = archive.open(archive.infolist()[0]).readlines()
+random_state = np.random.RandomState(1999)
+random_state.shuffle(raw)
 cats = [l.split("\t")[0] for l in raw]
 labels = [0 if l == "ham" else 1 for l in cats]
-
 data = [l.split("\t")[1].rstrip() for l in raw]
-tok = Tokenizer(preserve_case=False)
-tok_data = [tok.tokenize(s) for s in data]
 
-vocab = Counter()
-for tok_data_i in tok_data:
-    vocab.update(tok_data_i)
+tr = int(.8 * float(len(data)))
+train_data = data[:tr]
+train_labels = labels[:tr]
+#train_data, train_labels, _ = balanced_sample_maker(train_data, train_labels, random_state=random_state)
 
-lookup = defaultdict(lambda: 0)
-vocab_size = 8000
+test_data = data[tr:]
+test_labels = labels[tr:]
+#test_data, test_labels, _ = balanced_sample_maker(test_data, test_labels, random_state=random_state)
+
+tfidf = TfidfVectorizer()
+tfidf.fit(train_data)
+vocab_size = len(tfidf.get_feature_names())
 n_classes = 2
 
-# 0 is UNK
-for n, (k, v) in enumerate(vocab.most_common(vocab_size - 1)):
-    lookup[k] = n + 1
+def feature_fn(x):
+    transformed = tfidf.transform([x])
+    return transformed.nonzero()[1]
 
-def feature_fn(s):
-    si = tok.tokenize(s)
-    indices = [lookup[sii] for sii in si]
-    return indices
-
-"""
-tr = int(.8 * float(len(data)))
-train_data = data[:tr]
-train_labels = labels[:tr]
-
-test_data = data[tr:]
-test_labels = labels[tr:]
-
-m1 = MaxEnt(feature_fn, n_features=vocab_size,
-            n_classes=n_classes)
-m2 = SMaxEnt(feature_fn, n_features=vocab_size,
-             n_classes=n_classes)
-
-nll1 = m1._cost(train_data, train_labels, 0.)
-nll2 = m2._cost(train_data, train_labels, 0.)
-
-grads1 = m1._grads(train_data, train_labels, 0.)
-print(grads1.sum())
-grads2 = m2._grads(train_data, train_labels, 0.)
-print(grads2.sum())
-'''
-"""
-model = SMaxEnt(feature_fn, n_features=vocab_size, n_classes=n_classes)
-tr = int(.8 * float(len(data)))
-train_data = data[:tr]
-train_labels = labels[:tr]
-
-test_data = data[tr:]
-test_labels = labels[tr:]
+model = SparseMaxEnt(feature_fn, n_features=vocab_size, n_classes=n_classes,
+                     random_state=random_state)
 
 model.fit(train_data, train_labels, 0.)
-raise ValueError()
 def do_eval(d):
     probs = model.predict_proba(d)
     preds = np.argmax(probs, axis=-1)
@@ -70,5 +79,8 @@ def do_eval(d):
     spams = [dd for n, dd in enumerate(d) if n in np.where(preds == 1)[0]]
     return preds, probs, hams, spams
 
-test_preds, test_probs, hams, spams = do_eval(test_data)
+train_preds, train_probs, train_hams, train_spams = do_eval(train_data)
+print("Train accuracy {}".format(np.mean([tl == tp for tl, tp in zip(train_labels, train_preds)])))
+test_preds, test_probs, test_hams, test_spams = do_eval(test_data)
+print("Test accuracy {}".format(np.mean([tl == tp for tl, tp in zip(test_labels, test_preds)])))
 from IPython import embed; embed(); raise ValueError()
